32f89235e25cd5865ad1d5794156a571c492c85d
WindFlyCloud <caoxu@521.com>
2016-03-12 09:33:54 +0800

    Revert "2.0"
    This reverts commit 221663234b69692b67f2aff2ea9dd1a79ee61841.

--- Example/Pods/Headers/Public/ETSegmentedView/ETSegmentedView.h
+++
@@ -1,1 +0,0 @@
-../../../../../Pod/Classes/ETSegmentedView.h
--- Example/Pods/Target Support Files/ETSegmentedView/ETSegmentedView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ETSegmentedView : NSObject
-@end
-@implementation PodsDummy_ETSegmentedView
-@end
